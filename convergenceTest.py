67251f11feaa2bf4f2a3fe58fe8485ab14f33a40
Brendan Benshoof <brendan@glidr.net>
2015-10-13 11:42:34 -0400

    Logic template is up. please sanity check.

--- convergenceTest.py
+++ convergenceTest.py
@@ -1,5 +1,43 @@
 import networkx
 import random
+import threading
+
+
+class Logic(object):
+
+    def __init__(self, mid, dist, peermin, peermax):
+        self.midfunc = mid
+        self.distfunc = dist
+        self.short_peer_min = peermin
+        self.long_peer_max = peermax
+
+    def peerFilter(self, center, others):
+        # DGVH
+        if(len(others) <= self.short_peer_min):
+            return others, []
+        candidates = sorted(
+            others, key=lambda x: self.distfunc(center.loc, x.loc))
+        selected = [candidates[0]]
+        candidates = candidates[1:]
+        extra = []
+        for c in candidates:
+            m = self.midfunc(center.loc, c.loc)
+            rejected = False
+            mydist = self.distfunc(center.loc, m)
+            for p in selected:
+                if self.distfunc(p.loc, m) < mydist:
+                    rejected = True
+                    break
+            if rejected:
+                extra.append(p)
+            else:
+                selected.append(p)
+        if(len(selected) < self.short_peer_min):
+            selected = selected + extra[:self.short_peer_min - len(selected)]
+            extra = extra[self.short_peer_min - len(selected):]
+        if len(extra) < self.long_peer_max:
+            extra = random.sample(extra, self.long_peer_max)
+        return selected, extra
 
 
 class Node(object):
@@ -9,17 +47,21 @@ class Node(object):
         self.short_peers = []
         self.long_peers = []
         self.notified = []
+        self.PeerLock = threading.Lock()
         self.logic = dhtLogic
 
     def join(self, bootstraps):
-        self.short_peers, self.long_peers = self.logic.peerFilter(
-            self.loc, bootstraps)
+        with self.PeerLock():
+            self.short_peers, self.long_peers = self.logic.peerFilter(
+                self.loc, bootstraps)
 
     def getPeers(self):
-        return self.short_peers[:]
+        with self.PeerLock():
+            return self.short_peers[:]
 
     def notify(self, other):
-        self.notified.append(other)
+        with self.PeerLock():
+            self.notified.append(other)
 
     def tick(self):
         new_pool = set(self.short_peers + self.long_peers)
@@ -27,8 +69,10 @@ class Node(object):
             new_peers = p.getPeers()
             new_pool.update(set(new_peers))
         new_pool.update(set(self.notified))
-        self.notified = []
-        self.short_peers, self._long_peers = self.logic.peerFilter(
-            self.loc, list(new_pool))
+        with self.PeerLock():
+            self.notified = []
+            self.short_peers, self._long_peers = self.logic.peerFilter(
+                self.loc, list(new_pool))
+
         for p in self.short_peers:
             p.notify(self)
